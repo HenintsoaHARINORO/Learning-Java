4a9e779061aa1d863d0eb51c3d490b5b1d11d7ed
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-07-05 14:27:49 +0300

    creating a new instance with the subclass

--- Chapter3/TestTax.java
+++ Chapter3/TestTax.java
@@ -1,17 +1,21 @@
 
  class TestTax {
       public static void main(String [] args) {
-    	  Tax t= new Tax(); // creating an instance
+    	  /* Tax t= new Tax(); // creating an instance
     	  
     	  // assigning the values to class members
     	  t.grossIncome= 5000;
     	  t.dependents= 2;
     	  t.state= "NJ";
     	  
+    	  double yourTax= t.calcTax(); // calculating tax 
+    	  */
     	  
-    	  
-    	  double yourTax= t.calcTax(); // calculating tax
+    	  NJTax t = new NJTax(); // creating a new instance from the subclass
+    	  double yourTax= t.calcTax();
+    	  double totalTax= t.adjustForStudents(yourTax);
     	  
     	  System.out.println(" Your tax is "+ yourTax);
+    	  System.out.println(" Your tax total is " + totalTax);
       }
 }
