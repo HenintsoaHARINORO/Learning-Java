24ae51f0491b9dcfd407102baf10d5720770f824
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-07-05 11:57:23 +0300

    new class

---
+++ FriendsAndFamily.java
@@ -0,0 +1,7 @@
+public class FriendsAndFamily
+{
+   public static void main(String [] args){
+      System.out.println("Your favorite store runs a 30% off sale on T-shirts in this summer");
+      System.out.println("Your favorite store runs a 30% off sale on rice and vegetables");    
+   }
+}
