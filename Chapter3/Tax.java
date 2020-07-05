1097efd024d1f78134af0c5446509b4c599663c7
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-07-05 14:46:20 +0300

    formating codes

--- Chapter3/Tax.java
+++ Chapter3/Tax.java
@@ -1,26 +1,24 @@
 
 public class Tax {
-     double grossIncome;
-     String state;
-     int dependents;
-     
-     public double calcTax()
-     {
-    	 double stateTax= 0;
-    	 if (grossIncome < 30000) {
-    	 stateTax=grossIncome*0.05;
-    	 }
-    	 else{
-    	 stateTax= grossIncome*0.06;
-    	 }
-    	 return stateTax;
-     }
+	double grossIncome;
+	String state;
+	int dependents;
+
+	public double calcTax() {
+		double stateTax = 0;
+		if (grossIncome < 30000) {
+			stateTax = grossIncome * 0.05;
+		} else {
+			stateTax = grossIncome * 0.06;
+		}
+		return stateTax;
+	}
 }
 
-class NJTax extends Tax{
-     double adjustForStudents (double stateTax){
-      double adjustedTax = stateTax - 500;
-      
-      return adjustedTax;
-   }
+class NJTax extends Tax {
+	double adjustForStudents(double stateTax) {
+		double adjustedTax = stateTax - 500;
+
+		return adjustedTax;
+	}
 }
