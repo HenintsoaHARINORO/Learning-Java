5db585ee9957e1cd04f52570cd66b86d4292d42d
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2020-07-06 12:09:12 +0300

    removing useless comments

--- Chapter6/com/practicaljava/lesson6/Contractor.java
+++ Chapter6/com/practicaljava/lesson6/Contractor.java
@@ -4,11 +4,11 @@ public class Contractor extends Person implements Payable {
 
 	public Contractor(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public boolean increasePay(int percent) {
-		// TODO Auto-generated method stub
+		
 		if (percent < INCREASE_CAP) {
 			System.out.println("Increasing salary by " + percent + "%. " + getName());
 		} else {
--- Chapter6/com/practicaljava/lesson6/Employee.java
+++ Chapter6/com/practicaljava/lesson6/Employee.java
@@ -5,11 +5,11 @@ public class Employee extends Person implements Payable {
 	
 	public Employee(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public boolean increasePay(int percent) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Increasing salary by "+ percent +"%. " + getName());
 		return true;
 	}
